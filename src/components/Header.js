1647ab9d48ac052c88d50de225711181257e3b9e
Gogueta-Maximo <antonycastano745@gmail.com>
2025-06-12 13:54:17 -0500

    enter en header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,9 @@ import React from 'react';
 function Header() {
   return (
     <header>
-      <h1>Gestión de Mensualidades</h1>
+      <h1>Gestión de Mensualidades
+        
+      </h1>
     </header>
   );
 }
